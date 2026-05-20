4c78c737343318b3b298caf76ab73bfc3ac102ce
Rytham <frellancerytham@gmail.com>
2026-05-20 11:06:38 +0530

    Backend cors and start scripts prepared

--- AI-powered-E-Commerce-Website/backend/index.js
+++ AI-powered-E-Commerce-Website/backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://ecom-ai-vork.onrender.com"],
  credentials:true
 }))
 
